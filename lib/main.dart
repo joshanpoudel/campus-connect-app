08338fed2729fcb9b87ed1881f9e8640c131199d
Joshan Poudel <josanpoudel444@gmail.com>
2023-05-18 18:09:48 +0545

    implement courses

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:sample_app/Course.dart';
+import 'package:sample_app/Dashboard.dart';
 import 'Home.dart';
 import 'Information.dart';
 import 'Details.dart';
+import 'Dashboard.dart';
+import 'Course.dart';
+import 'Semester.dart';
 
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
@@ -13,7 +18,7 @@ class MyApp extends StatelessWidget {
       title: 'My Flutter App',
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
-      home: Details()
+      home: Semester()
 
     );
   }
